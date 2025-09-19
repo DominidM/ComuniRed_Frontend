dc260dafa4b827bcac0a24b896c2a38d5aa7bcd8
Dominid Muñoz Eslava <dominidzero@gmail.com>
2025-09-19 00:35:26 -0500

    [DME] 19092025 - css update

--- ComuniRed/src/app/app.config.ts
+++ ComuniRed/src/app/app.config.ts
@@ -1,10 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
+import { provideApollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideApollo(() => ({
+      cache: new InMemoryCache(),
+      link: inject(HttpLink).create({ 
+        uri: 'http://localhost:8080/graphql' 
+      }),
+    })),
   ]
 };
--- ComuniRed/src/app/services/rol.service.ts
+++ ComuniRed/src/app/services/rol.service.ts
@@ -4,67 +4,44 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export interface Rol {
-  id: number;
   nombre: string;
-  descripcion?: string;
+  descripcion: string;
+  id?: string;
+}
+
+export interface RolPage {
+  content: Rol[];
+  totalPages: number;
+  totalElements: number;
+  number: number;
+  size: number;
 }
 
 @Injectable({ providedIn: 'root' })
 export class RolService {
   constructor(private apollo: Apollo) {}
 
-  getAll(): Observable<Rol[]> {
-    return this.apollo.query<{ roles: Rol[] }>({
+  obtenerRoles(page: number, size: number): Observable<RolPage> {
+    return this.apollo.query<{ obtenerRoles: RolPage }>({
       query: gql`
-        query {
-          roles {
-            id
-            nombre
-            descripcion
-          }
-        }
-      `
-    }).pipe(map(result => result.data.roles));
-  }
-
-  create(rol: Partial<Rol>): Observable<Rol> {
-    return this.apollo.mutate<{ createRol: Rol }>({
-      mutation: gql`
-        mutation($input: RolInput!) {
-          createRol(input: $input) {
-            id
-            nombre
-            descripcion
+        query ($page: Int!, $size: Int!) {
+          obtenerRoles(page: $page, size: $size) {
+            content {
+              id
+              nombre
+              descripcion
+            }
+            totalPages
+            totalElements
+            number
+            size
           }
         }
       `,
-      variables: { input: rol }
-    }).pipe(map(result => result.data!.createRol));
-  }
-
-  update(id: number, rol: Partial<Rol>): Observable<Rol> {
-    return this.apollo.mutate<{ updateRol: Rol }>({
-      mutation: gql`
-        mutation($id: ID!, $input: RolInput!) {
-          updateRol(id: $id, input: $input) {
-            id
-            nombre
-            descripcion
-          }
-        }
-      `,
-      variables: { id, input: rol }
-    }).pipe(map(result => result.data!.updateRol));
-  }
-
-  delete(id: number): Observable<boolean> {
-    return this.apollo.mutate<{ deleteRol: boolean }>({
-      mutation: gql`
-        mutation($id: ID!) {
-          deleteRol(id: $id)
-        }
-      `,
-      variables: { id }
-    }).pipe(map(result => result.data!.deleteRol));
+      variables: {
+        page,
+        size
+      }
+    }).pipe(map(result => result.data?.obtenerRoles as RolPage));
   }
 }
