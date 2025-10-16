a2c1ef15fb8e5a22a558dedc1c9df393e0f6cd24
Blanca Isabel Joaquin H. <114704227+Isabeljh31@users.noreply.github.com>
2025-10-16 01:01:33 -0500

    Notificaciones
    Falta agregar botones

--- ComuniRed/src/app/public/notifications/notifications.component.ts
+++ ComuniRed/src/app/public/notifications/notifications.component.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './notifications.component.css'
 })
 export class NotificationsComponent {
-
+  notifications = [
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/1946/1946429.png',
+      text: 'Solo',
+      time: '10 minutos'
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/847/847969.png',
+      text: 'Hola Prueba 1.',
+      time: '3 horas'
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968841.png',
+      text: 'Sola',
+      time: '4 horas'
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/847/847969.png',
+      text: 'Hola',
+      time: '4 horas'
+    }
+  ];
 }
